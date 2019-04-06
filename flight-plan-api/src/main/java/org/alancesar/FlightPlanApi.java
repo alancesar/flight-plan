ca3c1d8c2b28700b5ce62f0500e003edfe61e274
Alan Cesar <alancesarelias@gmail.com>
2019-04-05 22:42:25 -0300

    Refactoring

--- flight-plan-api/src/main/java/org/alancesar/FlightPlanApi.java
+++ flight-plan-api/src/main/java/org/alancesar/FlightPlanApi.java
@@ -1,12 +1,37 @@
 package org.alancesar;
 
+import org.alancesar.itinerary.FullItineraryNameGenerator;
+import org.alancesar.itinerary.ItineraryNameGenerator;
+import org.alancesar.repository.CsvRouteRepository;
+import org.alancesar.service.RouteService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class FlightPlanApi {
 
     public static void main(String[] args) {
         SpringApplication.run(FlightPlanApi.class, args);
+    }
+
+    @Bean
+    public ItineraryNameGenerator getItineraryNameGenerator() {
+        return new FullItineraryNameGenerator(">>");
+    }
+
+    @Bean
+    public RouteService getRouteService() {
+        try {
+            String path = new ClassPathResource("routes.csv").getFile().getAbsolutePath();
+            CsvRouteRepository repository = new CsvRouteRepository(path);
+            return new RouteService(repository);
+        } catch (IOException e) {
+        }
+
+        return null;
     }
 }
--- flight-plan-api/src/main/java/org/alancesar/controller/FlightPlanController.java
+++ flight-plan-api/src/main/java/org/alancesar/controller/FlightPlanController.java
@@ -1,15 +1,16 @@
 package org.alancesar.controller;
 
-import org.alancesar.itinerary.FullItineraryNameGenerator;
+import org.alancesar.itinerary.ItineraryNameGenerator;
 import org.alancesar.itinerary.ItineraryProcessor;
 import org.alancesar.model.BestRoute;
 import org.alancesar.model.Itinerary;
 import org.alancesar.model.Route;
-import org.alancesar.repository.CsvRouteRepository;
 import org.alancesar.route.BestRouteProcessor;
 import org.alancesar.route.RouteProcessor;
 import org.alancesar.service.RouteService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,23 +19,36 @@ import java.util.List;
 @RestController
 public class FlightPlanController {
 
-    @GetMapping("/bestRoute")
+    private ItineraryNameGenerator itineraryNameGenerator;
+    private RouteService service;
+
+    public FlightPlanController(ItineraryNameGenerator itineraryNameGenerator, RouteService service) {
+        this.itineraryNameGenerator = itineraryNameGenerator;
+        this.service = service;
+    }
+
+    @GetMapping("/routes/best")
     public BestRoute getBestRoute(@RequestParam("origin") String origin,
-                             @RequestParam("destination") String destination) {
+                                  @RequestParam("destination") String destination) {
 
-        //TODO
-
-        CsvRouteRepository repository = new CsvRouteRepository("/Users/acesar/avenuecode/flight-plan/flight-plan-api/src/main/resources/routes.csv");
-        RouteService service = new RouteService(repository);
         List<Route> routes = service.getRoutes();
         ItineraryProcessor processor = new ItineraryProcessor(routes);
 
         List<Itinerary> starter = processor.findStarterItineraries(origin, destination);
         List<Itinerary> itineraries = processor.findItineraries(starter, destination);
 
-        RouteProcessor routeProcessor = new BestRouteProcessor(new FullItineraryNameGenerator(">>"));
+        if (itineraries.isEmpty()) {
+            return null;
+        }
+
+        RouteProcessor routeProcessor = new BestRouteProcessor(itineraryNameGenerator);
         BestRoute bestRoute = routeProcessor.find(itineraries);
 
         return bestRoute;
+    }
+
+    @PostMapping("/routes")
+    public void addRoute(@RequestBody  Route route) {
+        service.save(route);
     }
 }
